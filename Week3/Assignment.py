b6578449c61c850c8370b44a6221df7cd767e6a5
Jose <antoniusjose0126@gmail.com>
2023-10-05 17:31:46 +0700

    Update Assignment.py

--- Week3/Assignment.py
+++ Week3/Assignment.py
@@ -1,10 +1,11 @@
 
 L1cc = []
 person1 = {
-    "First_name":"Joshua",
-    "Last_name":"Rawatan",
-    "Age":"18",
-}
+    "First_name":"Marcelinus",
+    "Last_name":"Luviandanu",
+    "Age":"20",
+    "Hobby":"Wanker",
+    }
 
 L1cc.append(person1)
 
@@ -13,5 +14,5 @@ L1cc.append(person1)
 i = 1
 
 for person in L1cc:
-    print(str(i)+". name: " + person["First_name"],"\nage: " +person["Age"])
+    print(str(i)+". name: " + person["First_name"],"\nage: " +person["Age"],"\nhobby: " +person["Hobby"])
     i+=1
