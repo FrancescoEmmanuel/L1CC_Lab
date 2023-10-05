817417a837197f08d48e6cb426dd060214b9771d
rlaxNstdy <144894046+rlaxNstdy@users.noreply.github.com>
2023-10-05 17:36:49 +0700

    Update Assignment.py
    ....

--- Week3/Assignment.py
+++ Week3/Assignment.py
@@ -1,15 +1,9 @@
 
 L1cc = []
 person1 = {
-<<<<<<< HEAD
-    "First_name":"Vammy",
-    "Last_name":"Jiang",
-    "Age":"17",
-=======
-    "First_name":"Almira",
-    "Last_name":"Rana",
+    "First_name":"Azka",
+    "Last_name":"Azhad",
     "Age":"19",
->>>>>>> c2d03833e8194e0e71b40f410b761ebdfa978db2
 }
 
 L1cc.append(person1)
